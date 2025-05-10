04ce30ebd010d2aa926c28511a358cb6180c067f
doobiegodwin <godwinnbu@gmail.com>
2025-05-10 14:35:19 -0400

    added my name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("I am writing my first C# code :) ");
+
+        Console.Write("What is your firstname?  ");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your lasttname?  ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
