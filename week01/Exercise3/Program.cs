b2c555cb7b1caa783751f3c1a883324c3952a8bc
doobiegodwin <godwinnbu@gmail.com>
2025-05-10 16:20:20 -0400

    guessing game added

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomNum = new Random();
+        int magicNum = randomNum.Next(1, 100);
+
+        int guess = 0;
+
+        while (guess != magicNum)
+        {
+
+            Console.Write("What is your guess?  ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == magicNum)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess < magicNum)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else
+            {
+                Console.WriteLine("Lower!");
+            }
+        }
     }
 }
